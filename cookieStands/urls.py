5a02f0c0ef1a21874e892e457b73814c79dcac38
mikeascalon <mikeascalon@yahoo.com>
2024-03-04 14:31:28 -0800

    edited urls.py in cookieStand

--- cookieStands/urls.py
+++ cookieStands/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views_front import CookieStandListView, CookieStandDetailView, CookieStandUpdateView, CookieStandCreateView, CookieStandDeleteView
 
 urlpatterns = [
-    path('cookieStands/', CookieStandListView.as_view(), name='cookieStand_list'),
+    path('', CookieStandListView.as_view(), name='cookieStand_list'),
     path('cookieStands/<int:pk>/', CookieStandDetailView.as_view(), name='cookieStand_detail'),
     path('cookieStands/<int:pk>/update/', CookieStandUpdateView.as_view(), name='cookieStand_update'),
     path('cookieStands/create/', CookieStandCreateView.as_view(), name='cookieStand_create'),
